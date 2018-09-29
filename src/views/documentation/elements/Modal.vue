511b763fdc0faad6ccd88367154503342a3d0a22
ovinsyah <oviensyahalbayhaqy@gmail.com>
2018-09-29 14:34:47 +0700

    modal

--- src/views/documentation/elements/Modal.vue
+++ src/views/documentation/elements/Modal.vue
@@ -33,7 +33,7 @@
                 <div class="area selected">
                     <div>&#x3C;button class=&#x22;ini button blue openModal&#x22; data-target=&#x22;#modalSmall&#x22;&#x3E;Open Modal&#x3C;/button&#x3E;</div>
                     <div>&#x3C;div class=&#x22;ini modals&#x22; id=&#x22;modalSmall&#x22;&#x3E;</div>
-                        <div class="ml-4">&#x3C;div class=&#x22;ini modal medium&#x22;&#x3E;</div>
+                        <div class="ml-4">&#x3C;div class=&#x22;ini modal small&#x22;&#x3E;</div>
                             <div class="ml-5">&#x3C;div class=&#x22;header&#x22;&#x3E;</div>
                                 <div class="ml-6">&#x3C;h3&#x3E;Header Title&#x3C;/h3&#x3E;</div>
                                 <div class="ml-6">&#x3C;button class=&#x22;ini button close closeModal&#x22;&#x3E;&#x3C;i class=&#x22;material-icons&#x22;&#x3E;close&#x3C;/i&#x3E;&#x3C;/button&#x3E;</div>
@@ -116,7 +116,70 @@
                 <textarea cols="100" rows="100" class="hidden">
 &#x3C;button class=&#x22;ini button inicss openModal&#x22; data-target=&#x22;#modalMedium&#x22;&#x3E;Open Modal&#x3C;/button&#x3E;
 &#x3C;div class=&#x22;ini modals&#x22; id=&#x22;modalMedium&#x22;&#x3E;
-    &#x3C;div class=&#x22;ini modal small&#x22;&#x3E;
+    &#x3C;div class=&#x22;ini modal medium&#x22;&#x3E;
+        &#x3C;div class=&#x22;header&#x22;&#x3E;
+            &#x3C;h3&#x3E;Header Title&#x3C;/h3&#x3E;
+            &#x3C;button class=&#x22;ini button close closeModal&#x22;&#x3E;&#x3C;i class=&#x22;material-icons&#x22;&#x3E;close&#x3C;/i&#x3E;&#x3C;/button&#x3E;
+        &#x3C;/div&#x3E;
+        &#x3C;div class=&#x22;content&#x22;&#x3E;
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta blanditiis ducimus quis, exercitationem a? Qui iusto deserunt necessitatibus, quis cupiditate temporibus id corrupti eligendi expedita possimus laboriosam totam blanditiis!
+        &#x3C;/div&#x3E;
+        &#x3C;div class=&#x22;action&#x22;&#x3E;
+            &#x3C;button class=&#x22;ini button midnight&#x22;&#x3E;Okay&#x3C;/button&#x3E;
+            &#x3C;button class=&#x22;ini button red closeModal&#x22;&#x3E;Cancel&#x3C;/button&#x3E;
+        &#x3C;/div&#x3E;
+    &#x3C;/div&#x3E;
+&#x3C;/div&#x3E;</textarea>
+            </div>
+          </div>
+          <h4 class="mt-3">Modal Large</h4>
+          <div class="ini section">
+            <div class="example container">
+                <button class="ini button inicss openModal" data-target="#modalLarge">Open Modal</button>
+                <div class="ini modals" id="modalLarge">
+                    <div class="ini modal large">
+                        <div class="header">
+                            <h3>Header Title</h3>
+                            <button class="ini button close closeModal"><i class="material-icons">close</i></button>
+                        </div>
+                        <div class="content">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta blanditiis ducimus quis, exercitationem a? Qui iusto deserunt necessitatibus, quis cupiditate temporibus id corrupti eligendi expedita possimus laboriosam totam blanditiis!
+                        </div>
+                        <div class="action">
+                            <button class="ini button midnight">Okay</button>
+                            <button class="ini button red closeModal">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="code">
+                <div class="ini align right">
+                    <button class="ini button copy onCopy">Copy</button>
+                </div>
+                <div class="area selected">
+                    <div>&#x3C;button class=&#x22;ini button blue openModal&#x22; data-target=&#x22;#modalLarge&#x22;&#x3E;Open Modal&#x3C;/button&#x3E;</div>
+                    <div>&#x3C;div class=&#x22;ini modals&#x22; id=&#x22;modalLarge&#x22;&#x3E;</div>
+                        <div class="ml-4">&#x3C;div class=&#x22;ini modal large&#x22;&#x3E;</div>
+                            <div class="ml-5">&#x3C;div class=&#x22;header&#x22;&#x3E;</div>
+                                <div class="ml-6">&#x3C;h3&#x3E;Header Title&#x3C;/h3&#x3E;</div>
+                                <div class="ml-6">&#x3C;button class=&#x22;ini button close closeModal&#x22;&#x3E;&#x3C;i class=&#x22;material-icons&#x22;&#x3E;close&#x3C;/i&#x3E;&#x3C;/button&#x3E;</div>
+                            <div class="ml-5">&#x3C;/div&#x3E;</div>
+                            <div class="ml-5">&#x3C;div class=&#x22;content&#x22;&#x3E;</div>
+                                <div class="ml-6">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum soluta blanditiis ducimus quis, exercitationem a? Qui iusto deserunt necessitatibus, quis cupiditate temporibus id corrupti eligendi expedita possimus laboriosam totam blanditiis!
+                                </div>
+                            <div class="ml-5">&#x3C;/div&#x3E;</div>
+                            <div class="ml-5">&#x3C;div class=&#x22;action&#x22;&#x3E;</div>
+                                <div class="ml-6">&#x3C;button class=&#x22;ini button midnight&#x22;&#x3E;Okay&#x3C;/button&#x3E;</div>
+                                <div class="ml-6">&#x3C;button class=&#x22;ini button red closeModal&#x22;&#x3E;Cancel&#x3C;/button&#x3E;</div>
+                            <div class="ml-5">&#x3C;/div&#x3E;</div>
+                        <div class="ml-4">&#x3C;/div&#x3E;</div>
+                    <div>&#x3C;/div&#x3E;</div>
+                </div>
+                <textarea cols="100" rows="100" class="hidden">
+&#x3C;button class=&#x22;ini button inicss openModal&#x22; data-target=&#x22;#modalLarge&#x22;&#x3E;Open Modal&#x3C;/button&#x3E;
+&#x3C;div class=&#x22;ini modals&#x22; id=&#x22;modalLarge&#x22;&#x3E;
+    &#x3C;div class=&#x22;ini modal large&#x22;&#x3E;
         &#x3C;div class=&#x22;header&#x22;&#x3E;
             &#x3C;h3&#x3E;Header Title&#x3C;/h3&#x3E;
             &#x3C;button class=&#x22;ini button close closeModal&#x22;&#x3E;&#x3C;i class=&#x22;material-icons&#x22;&#x3E;close&#x3C;/i&#x3E;&#x3C;/button&#x3E;
