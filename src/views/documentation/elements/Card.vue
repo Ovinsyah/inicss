0f51864a0d8229f901f03a1b523037ea363155ff
witech <witech@witechs-iMac.local>
2018-10-03 11:58:54 +0700

    Update card

--- src/views/documentation/elements/Card.vue
+++ src/views/documentation/elements/Card.vue
@@ -152,8 +152,73 @@
                 </div>
               </div>
               <div class="card footer">
-                <button class="ini button blue">Button Left</button>
-                <button class="ini button blue float right">Button Right</button>
+                <button class="ini button o-green mr-1">Approve</button>
+                <button class="ini button o-red">Deny</button>
+              </div>
+            </div>
+          </div>
+          <div class="code">
+              <div class="ini align right">
+                  <button class="ini button copy onCopy">Copy</button>
+              </div>
+              <div class="area selected">
+                <div>
+                  <div><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;ini card&#x22;</span><span class="tag">&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card content&#x22;</span><span class="tag">&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;div&#x3E;</span>Header<span class="tag">&#x3C;/div&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;/div&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card image&#x22;</span><span class="tag">&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;img</span> src=&#x22;...&#x22; alt=&#x22;image&#x22; <span class="tag">/&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;/div&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card content&#x22;</span><span class="tag">&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card title&#x22;</span>&#x3E;Card Title<span class="tag">&#x3C;/div&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card subtitle&#x22;</span>&#x3E;Card Subtitle<span class="tag">&#x3C;/div&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card text&#x22;</span><span class="tag">&#x3E;</span></div>
+                        <div class="ml-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nesciunt eligendi distinctio dolorum vel? Numquam nihil molestias repellat quasi dolorum? Dolor porro nesciunt optio. Odio amet harum cupiditate quas unde.</div>
+                      <div class="ml-5"><span class="tag">&#x3C;/div&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;/div&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;div</span> <span class="class"> class=</span><span class="nclass">&#x22;card footer&#x22;</span><span class="tag">&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;button</span> <span class="class"> class=</span><span class="nclass">&#x22;ini button blue&#x22;</span>&#x3E;Button Left<span class="tag">&#x3C;/button&#x3E;</span></div>
+                      <div class="ml-5"><span class="tag">&#x3C;button</span> <span class="class"> class=</span><span class="nclass">&#x22;ini button blue float right&#x22;</span>&#x3E;Button Right<span class="tag">&#x3C;/button&#x3E;</span></div>
+                    <div class="ml-4"><span class="tag">&#x3C;/div&#x3E;</span></div>
+                  <div><span class="tag">&#x3C;/div&#x3E;</span></div>
+                </div>
+              </div>
+              <textarea cols="100" rows="100" class="hidden">
+&#x3C;div class=&#x22;ini card&#x22;&#x3E;
+  &#x3C;div class=&#x22;card content&#x22;&#x3E;
+    &#x3C;div&#x3E;Header&#x3C;/div&#x3E;
+  &#x3C;/div&#x3E;
+  &#x3C;div class=&#x22;card image&#x22;&#x3E;
+    &#x3C;img src=&#x22;...&#x22; alt=&#x22;image&#x22; /&#x3E;
+  &#x3C;/div&#x3E;
+  &#x3C;div class=&#x22;card content&#x22;&#x3E;
+    &#x3C;div class=&#x22;card title&#x22;&#x3E;Card Title&#x3C;/div&#x3E;
+    &#x3C;div class=&#x22;card subtitle&#x22;&#x3E;Card Subtitle&#x3C;/div&#x3E;
+    &#x3C;div class=&#x22;card text&#x22;&#x3E;
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nesciunt eligendi distinctio dolorum vel? Numquam nihil molestias repellat quasi dolorum? Dolor porro nesciunt optio. Odio amet harum cupiditate quas unde.
+    &#x3C;/div&#x3E;
+  &#x3C;/div&#x3E;
+  &#x3C;div class=&#x22;card footer&#x22;&#x3E;
+    &#x3C;button class=&#x22;ini button blue&#x22;&#x3E;Button Left&#x3C;/button&#x3E;
+    &#x3C;button class=&#x22;ini button blue float right&#x22;&#x3E;Button Right&#x3C;/button&#x3E;
+  &#x3C;/div&#x3E;
+&#x3C;/div&#x3E;</textarea>
+          </div>
+
+          <div class="mb-3"></div>
+          <div class="example card-basic">
+            <div class="ini card">
+              <div class="card content">
+                <div class="card title">Card Title</div>
+                <div class="card subtitle">Card Subtitle</div>
+                <div class="card text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam nesciunt eligendi distinctio dolorum vel? Numquam nihil molestias repellat quasi dolorum? Dolor porro nesciunt optio. Odio amet harum cupiditate quas unde.
+                </div>
+              </div>
+              <div class="card footer">
+                <button class="ini button o-green mr-1">Approve</button>
+                <button class="ini button o-red">Deny</button>
               </div>
             </div>
           </div>
