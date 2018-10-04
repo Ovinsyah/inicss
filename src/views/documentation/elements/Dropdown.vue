028d08714b61a47b09720d042f11ba211f0275c9
uqbahharis <uqbahharisnst@ymail.com>
2018-10-04 12:26:57 +0700

    delete italic text

--- src/views/documentation/elements/Dropdown.vue
+++ src/views/documentation/elements/Dropdown.vue
@@ -5,6 +5,10 @@
       <Menu/>
       <div class="body content">
           <h1>Dropdown</h1>
+          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.</div>
       </div>
     </div>
   </div>
