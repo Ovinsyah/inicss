529005f7c27df45b29a3fe7ee2c6ed24360b2579
uqbahharis <uqbahharisnst@ymail.com>
2018-10-05 23:40:52 +0700

    add loader

--- src/views/documentation/collection/Loader.vue
+++ src/views/documentation/collection/Loader.vue
@@ -4,88 +4,71 @@
     <div class="ini wrapper container">
       <Menu/>
       <div class="body content">
-          <h1>Button</h1>
-          <div>Kayak gini kalo mau pakek button bos</div>
-          <h4 class="mt-3">Default Button</h4>
-          <div class="ini section">
-            <div class="example button">
-                <button class="ini button blue">Blue</button>
-                <button class="ini button black">Black</button>
-                <button class="ini button red">Red</button>
-                <button class="ini button green">Green</button>
-                <button class="ini button purple">Purple</button>
-                <button class="ini button carrot">Carrot</button>
-                <button class="ini button orange">Orange</button>
-                <button class="ini button gray">Gray</button>
-                <button class="ini button midnight">Midnight</button>
+        <h1>Loader</h1>
+        <div>Dokumentasi dan contoh penggunaan loader inicss</div>
+        <h4 class="mt-3">Default Progress Bar</h4>
+        <div class="ini section">
+          <div class="example progress">
+            <div class="ini progress">
+              <div class="bar" style="width: 0%;"></div>
             </div>
-            <div class="code">
-                <div class="ini align right">
-                    <button class="ini button copy onCopy">Copy</button>
-                </div>
-                <div class="area selected">
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button blue</span>&quot;<span class="tag">&gt;</span>Blue<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button black</span>&quot;<span class="tag">&gt;</span>Black<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button red</span>&quot;<span class="tag">&gt;</span>Red<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button green</span>&quot;<span class="tag">&gt;</span>Green<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button purple</span>&quot;<span class="tag">&gt;</span>Purple<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button carrot</span>&quot;<span class="tag">&gt;</span>Carrot<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button orange</span>&quot;<span class="tag">&gt;</span>Orange<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button gray</span>&quot;<span class="tag">&gt;</span>Gray<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button midnight</span>&quot;<span class="tag">&gt;</span>Midnight<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                </div>
-                <textarea cols="100" rows="100" class="hidden">
-&#x3C;button class=&#x22;ini button blue&#x22;&#x3E;Blue&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button black&#x22;&#x3E;Black&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button red&#x22;&#x3E;Red&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button green&#x22;&#x3E;Green&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button purple&#x22;&#x3E;Purple&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button carrot&#x22;&#x3E;Carrot&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button orange&#x22;&#x3E;Orange&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button gray&#x22;&#x3E;Gray&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button midnight&#x22;&#x3E;Midnight&#x3C;/button&#x3E;</textarea>
+            <div class="ini progress">
+              <div class="bar" style="width: 25%;">25%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar" style="width: 50%;">50%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar" style="width: 75%;">75%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar" style="width: 100%;">100%</div>
             </div>
           </div>
-          <h4 class="mt-3">Outline Button</h4>
-          <div class="ini section">
-            <div class="example button">
-                <button class="ini button o-blue">Blue</button>
-                <button class="ini button o-black">Black</button>
-                <button class="ini button o-red">Red</button>
-                <button class="ini button o-green">Green</button>
-                <button class="ini button o-purple">Purple</button>
-                <button class="ini button o-carrot">Carrot</button>
-                <button class="ini button o-orange">Orange</button>
-                <button class="ini button o-gray">Gray</button>
-                <button class="ini button o-midnight">Midnight</button>
+          <div class="code">
+
+            <textarea cols="100" rows="100" class="hidden">
+
+            </textarea>
+          </div>
+
+          <h4 class="mt-3">Colored Progress Bar</h4>
+          <div class="example progress">
+            <div class="ini progress">
+              <div class="bar blue" style="width: 25%;">25%</div>
             </div>
-            <div class="code">
-                <div class="ini align right">
-                    <button class="ini button copy onCopy">Copy</button>
-                </div>
-                <div class="area selected">
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-blue</span>&quot;<span class="tag">&gt;</span>Blue<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-black</span>&quot;<span class="tag">&gt;</span>Black<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-red</span>&quot;<span class="tag">&gt;</span>Red<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-green</span>&quot;<span class="tag">&gt;</span>Green<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-purple</span>&quot;<span class="tag">&gt;</span>Purple<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-carrot</span>&quot;<span class="tag">&gt;</span>Carrot<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-orange</span>&quot;<span class="tag">&gt;</span>Orange<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-gray</span>&quot;<span class="tag">&gt;</span>Gray<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div>
-                    <div><span class="tag">&lt;button</span> <span class="class"> class=</span><span class="nclass"> &quot;ini button o-midnight</span>&quot;<span class="tag">&gt;</span>Midnight<span class="tag">&lt;/button<span class="tag">&gt;</span></span></div> 
-                </div>
-                <textarea cols="100" rows="100" class="hidden">
-&#x3C;button class=&#x22;ini button o-blue&#x22;&#x3E;Blue&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-black&#x22;&#x3E;Black&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-red&#x22;&#x3E;Red&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-green&#x22;&#x3E;Green&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-purple&#x22;&#x3E;Purple&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-carrot&#x22;&#x3E;Carrot&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-orange&#x22;&#x3E;Orange&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-gray&#x22;&#x3E;Gray&#x3C;/button&#x3E;
-&#x3C;button class=&#x22;ini button o-midnight&#x22;&#x3E;Midnight&#x3C;/button&#x3E;</textarea>
+            <div class="ini progress">
+              <div class="bar black" style="width: 40%;">40%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar red" style="width: 75%;">75%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar green" style="width: 40%;">40%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar purple" style="width: 30%;">30%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar carrot" style="width: 60%;">60%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar orange" style="width: 20%;">20%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar gray" style="width: 80%;">80%</div>
+            </div>
+            <div class="ini progress">
+              <div class="bar midnight" style="width: 10%;">10%</div>
             </div>
           </div>
+          <div class="code">
+
+            <textarea cols="100" rows="100" class="hidden">
+
+            </textarea>
+          </div>
+        </div>
       </div>
     </div>
   </div>
